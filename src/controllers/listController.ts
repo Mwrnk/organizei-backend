32984fedc5630688756bf2fc08c9b71ec8164191
souzz2 <108484845+souzz2@users.noreply.github.com>
2025-05-01 15:13:50 -0300

    Criando o método que retorna as lista de um usuário

--- src/controllers/listController.ts
+++ src/controllers/listController.ts
@@ -40,6 +40,27 @@ export class ListController {
     });
   }
 
+  async getListByUserId(req: Request, res: Response): Promise<void> {
+    const { userId } = req.params;
+
+    const lists = await List.find({ userId });
+
+    if (!userId) {
+      throw new AppError("Usuário não encontrado", 404);
+    }
+    if (lists.length === 0) {
+      throw new AppError("Nenhuma lista encontrada para este usuário", 404);
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: lists.map((list) => ({
+        id: list._id,
+        name: list.name,
+      })),
+    });
+  }
+    
   async getListById(req: Request, res: Response): Promise<void> {
     const { id } = req.params;
     const list = await List.findById(id);
