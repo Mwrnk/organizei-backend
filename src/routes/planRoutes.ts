0fbea04745f6bd51815e31f347a27b72f34a2792
mts3324 <133476027+mts3324@users.noreply.github.com>
2025-05-04 17:38:20 -0300

    Atualização planRoutes Matheus Ribas

--- src/routes/planRoutes.ts
+++ src/routes/planRoutes.ts
@@ -12,5 +12,8 @@ router.get("/plans", (req, res) => planController.listPlans(req, res));
 router.post("/plans", authMiddleware, (req, res) => planController.createPlan(req, res));
 router.put("/users/:userId/plan", authMiddleware, (req, res) => planController.updateUserPlan(req, res));
 router.get("/users/:userId/plan-history", authMiddleware, (req, res) => planController.getUserPlanHistory(req, res));
+//FEITO POR MATHEUS RIBAS
+//ROTA PARA VERIFICAR O PLANO ATUAL DO USUARIO
+router.get("/users/:userId/plan", authMiddleware, (req, res) =>    planController.getUserCurrentPlan(req, res));
 
 export default router;
