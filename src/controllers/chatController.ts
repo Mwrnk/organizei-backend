184f259f2b91a324aff65b741ee1396dc8c7fc50
Matheus Ribas <matheusribas2013@gmail.com>
2025-06-17 00:48:31 -0300

    atualizacao no modelo

--- src/controllers/chatController.ts
+++ src/controllers/chatController.ts
@@ -6,18 +6,17 @@ dotenv.config();
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 export const chatWithAI = async (req: Request, res: Response) => {
-    const { message } = req.body;
-  
-    try {
-      const completion = await openai.chat.completions.create({
-        model: "gpt-4o-mini",
-        messages: [{ role: "user", content: message }],
-      });
-  
-      res.json({ response: completion.choices[0].message.content });
-    } catch (error: any) {
-      console.error("Erro ao se comunicar com a OpenAI:", error.message);
-      res.status(500).json({ error: error.message || "Erro desconhecido" });
-    }
-  };
-  
+  const { message } = req.body;
+
+  try {
+    const completion = await openai.chat.completions.create({
+      model: "gpt-4o",
+      messages: [{ role: "user", content: message }],
+    });
+
+    res.json({ response: completion.choices[0].message.content });
+  } catch (error: any) {
+    console.error("Erro ao se comunicar com a OpenAI:", error.message);
+    res.status(500).json({ error: error.message || "Erro desconhecido" });
+  }
+};
